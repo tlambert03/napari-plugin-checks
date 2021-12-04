16f746411162c96d2611443d14ee4068dc862b77
Talley Lambert <talley.lambert@gmail.com>
2021-12-04 11:32:15 -0500

    updates

---
+++ napari_plugin_checks.py
@@ -0,0 +1,162 @@
+"""Set of sanity checks for napari plugin
+
+use as a pre-commit hook, or command line interface.
+"""
+
+import argparse
+import logging
+import re
+from configparser import ConfigParser
+from fnmatch import fnmatch
+from pathlib import Path
+from typing import Callable, List, Optional, Sequence, Set, Tuple
+
+
+
+logging.getLogger("grimp").setLevel(logging.ERROR)
+
+Str2Bool = Callable[[str], bool]
+CHECKERS: List[Tuple[Str2Bool, Str2Bool]] = []
+BASE_NAME_REGEX = re.compile(r"[^!=><\s@~]+")
+REQ_REGEX = re.compile(r"(===|==|!=|~=|>=?|<=?|@)\s*([^,]+)")
+qtpy_message = "use Qt compatibility library, like qtpy"
+FORBIDDEN_REQUIRES = {
+    "pyqt": qtpy_message,
+    "pyqt5": qtpy_message,
+    "pyside": qtpy_message,
+    "pyside2": qtpy_message,
+    "pyside6": qtpy_message,
+    "pyqt6": qtpy_message,
+}
+FORBIDDEN_IMPORTS = {
+    "PyQt5": qtpy_message,
+    "PyQt6": qtpy_message,
+    "PySide2": qtpy_message,
+    "PySide6": qtpy_message,
+}
+
+
+def match(pattern: str) -> Callable[[Str2Bool], Str2Bool]:
+    """decorator that declares a checker for a filepattern:
+
+    Examples
+    --------
+        @match("setup.cfg")
+        def check_setup_cfg(fname: str) -> bool:
+            ...
+    """
+
+    def deco(f: Str2Bool) -> Str2Bool:
+        def _check_name(fname: str) -> bool:
+            if "/" not in pattern and "*" not in pattern:
+                return fname.endswith(pattern)
+            return fnmatch(fname, pat=pattern)
+
+        CHECKERS.append((_check_name, f))
+        return f
+
+    return deco
+
+
+def _req_base(req: str) -> str:
+    basem = re.match(BASE_NAME_REGEX, req.split(";")[0])
+    return basem.group(0).strip() if basem else ""
+
+
+@match("setup.cfg")
+def check_setup_cfg(fname: str) -> bool:
+    cfg = ConfigParser()
+    cfg.read(fname)
+
+    retv = False
+    requires = cfg.get("options", "install_requires", fallback="").strip().splitlines()
+    if requires:
+
+        # check for Forbidden dependencies, like PyQt5, etc...
+        for req in requires:
+            lib = _req_base(req)
+            err = FORBIDDEN_REQUIRES.get(lib.lower())
+            if err:
+                print(f"Forbidden dependency detected in setup.cfg ({lib!r}): {err}")
+                retv = True
+
+    return retv
+
+
+@match("requirements.txt")
+def check_requirements_txt(fname: str) -> bool:
+    retv = False
+    for line in Path(fname).read_text().strip().splitlines():
+        print("l", line)
+        lib = _req_base(line)
+        err = FORBIDDEN_REQUIRES.get(lib.lower())
+        if err:
+            print(f"Forbidden dependency detected in requirements.txt ({lib!r}): {err}")
+            retv = True
+    return retv
+
+
+@match("*.py")
+def check_py(fname: str) -> bool:
+    return _check_imports(fname)
+
+
+def _check_imports(fname: str) -> bool:
+    from grimp.adaptors import filesystem, importscanner
+    from grimp.domain.valueobjects import Module
+
+    p = Path(fname)
+    root = p.parent
+
+    module = Module(f"{p.parent.name}.{p.name[:-3]}")
+    fs = filesystem.FileSystem()
+    scanner = importscanner.ImportScanner({root: {module}}, fs, True)
+
+    imports = scanner.scan_for_imports(module=module)
+    retv = False
+    for imp in imports:
+        err = FORBIDDEN_IMPORTS.get(imp.imported.name)
+        if err:
+            print(
+                f"Forbidden import detected ({imp.imported.name!r}) in {fname!r}: {err}"
+            )
+            retv = True
+
+    return retv
+
+
+def check_file(filename: str) -> bool:
+    for checker, function in CHECKERS:
+        if checker(filename):
+            # the first checker wins, and must dispatch to others manually if desired.
+            return function(filename)
+    return False
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", nargs="*", default=["."])
+    args = parser.parse_args(argv)
+
+    for name in list(args.filenames):
+        p = Path(name)
+        if p.is_dir():
+            args.filenames.remove(name)
+            args.filenames.extend(
+                [
+                    str(f)
+                    for f in p.glob("**/*")
+                    if f.is_file()
+                    if not str(f).startswith(".")
+                ]
+            )
+
+    retv = 0
+    for filename in args.filenames:
+        if check_file(filename):
+            retv = 1
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
--- tests/test_a.py
+++ tests/test_a.py
@@ -1,2 +1,25 @@
-def test_a():
-    assert True
+from pathlib import Path
+from napari_plugin_checks import check_file
+import pytest
+
+
+def test_setup_cfg(capsys):
+    setup_cfg = Path(__file__).parent / "bad_examples" / "setup.cfg"
+    assert check_file(str(setup_cfg))
+    captured = capsys.readouterr()
+    assert "Forbidden dependency detected" in captured.out
+
+
+def test_requirements(capsys):
+    reqtxt = Path(__file__).parent / "bad_examples" / "requirements.txt"
+    assert check_file(str(reqtxt))
+    captured = capsys.readouterr()
+    assert "Forbidden dependency detected" in captured.out
+
+
+@pytest.mark.parametrize("fname", ["imports_pyqt5.py", "imports_pyside2.py"])
+def test_bad_imports(fname, capsys):
+    file = Path(__file__).parent / "bad_examples" / fname
+    assert check_file(str(file))
+    captured = capsys.readouterr()
+    assert "Forbidden import detected" in captured.out
